abd7cbaeee9f5f1c40dd0a19aee75170e0c76763
unknown <kipemail11@gmail.com>
2022-04-21 20:03:13 +0800

    first commit

---
+++ acara_29/lib/model/product.dart
@@ -0,0 +1,9 @@
+class Product {
+  final int id;
+  final String productNama;
+  final String productImage;
+  final String productDescription;
+  final double price;
+
+  Product(this.id, this.productNama, this.productImage, this.productDescription, this.price);
+}
